d6358e1750846dd3fee95bc0567e149955bf857b
KbrRamon <kbr1927@gmail.com>
2026-08-18 20:32:34 -0300

    feat: banco e migrations configurado

--- aluno/models.py
+++ aluno/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100)
+    curso = models.CharField(max_length=100)
+    bio = models.TextField(blank=True)
+    def __str__(self):
+        return self.nome
 
 # Create your models here.
--- aluno/views.py
+++ aluno/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Aluno
+def aluno(request):
+    alunos = Aluno.objects.all()
+    return render(request, 
+'aluno.html',
+        {'alunos': alunos})
 
 # Create your views here.
