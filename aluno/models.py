77b73a5ef88fd387946104f2641a4df1dfa2ea1c
KbrRamon <kbr1927@gmail.com>
2026-08-19 19:46:42 -0300

    feat: pagina aluno corrigida e cadastros OK

--- aluno/models.py
+++ aluno/models.py
@@ -3,6 +3,8 @@ class Aluno(models.Model):
     nome = models.CharField(max_length=100)
     curso = models.CharField(max_length=100)
     bio = models.TextField(blank=True)
+    
+    
     def __str__(self):
         return self.nome
 
