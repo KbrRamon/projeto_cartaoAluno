fb2b66b68738a58219a133bec9f44db284ad3d78
KbrRamon <kbr1927@gmail.com>
2026-08-26 20:12:47 -0300

    feat: CRUD finalizado

--- aluno/models.py
+++ aluno/models.py
@@ -4,8 +4,6 @@ class Aluno(models.Model):
     nome = models.CharField(max_length=100)
     curso = models.CharField(max_length=100)
     bio = models.TextField(blank=True)
-    idade = models.PositiveIntegerField(max_length=100)
-    matricula = models.CharField(max_length=20, unique=True)
     
     def __str__(self):
         return self.nome
--- aluno/views.py
+++ aluno/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Aluno
+
 def aluno(request):
     alunos = Aluno.objects.all()
-    return render(request, 
-'aluno.html',
-        {'alunos': alunos})
+    return render(request, 'aluno.html', {'alunos': alunos})
+
+def criar_aluno(request):
+    if request.method == 'POST':
+        nome  = request.POST['nome']
+        curso = request.POST['curso']
+        bio   = request.POST.get('bio', '')
+        Aluno.objects.create(nome=nome, curso=curso, bio=bio)
+        return redirect('aluno')
+    return render(request, 'aluno/form_aluno.html', {'titulo': 'Novo Aluno'})
+
+def editar_aluno(request, pk):
+    aluno = get_object_or_404(Aluno, pk=pk)
+    if request.method == 'POST':
+        aluno.nome  = request.POST['nome']
+        aluno.curso = request.POST['curso']
+        aluno.bio   = request.POST.get('bio', '')
+        aluno.save()
+        return redirect('aluno')
+    return render(request, 'aluno/form_aluno.html', {'aluno': aluno, 'titulo': f'Editar: {aluno.nome}'})
+
+def excluir_aluno(request, pk):
+    aluno = get_object_or_404(Aluno, pk=pk)
+    if request.method == 'POST':
+        aluno.delete()
+        return redirect('aluno')
+    return render(request, 'aluno/confirmar_exclusao.html', {'aluno': aluno})
 
 # Create your views here.
